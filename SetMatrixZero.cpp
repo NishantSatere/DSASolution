9a3de95524566584f8c2ebca493a2a7e878f933b
nishant <nishantd28satere@gmail.com>
2023-05-27 16:20:12 +0530

    Optimal Solution

--- SetMatrixZero.cpp
+++ SetMatrixZero.cpp
@@ -1,12 +1,15 @@
+// Set Matrix Zero
+// Problem Statement: Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+
 #include<bits/stdc++.h>
 using namespace std;
 
 
-//BRUTE FORCE
+//BRUTE FORCE SOLUTION
 void setMinusone(vector<vector<int>>& matrix){
 
     for(int i = 0; i < matrix.size(); i++){
-        for(int j = 0; j  < matrix.size(); j++){
+        for(int j = 0; j  < matrix[0].size(); j++){
             if(matrix[i][j] == 0){
                 int p = 0;
                 for(p = 0; p < matrix.size(); p++){
@@ -17,7 +20,7 @@ void setMinusone(vector<vector<int>>& matrix){
                 
 
                 int q = 0;
-                for(q = 0; q < matrix.size(); q++){
+                for(q = 0; q < matrix[0].size(); q++){
                     if(matrix[i][q] != 0){
                         matrix[i][q] = -1;
                     }
@@ -30,7 +33,7 @@ void setMinusone(vector<vector<int>>& matrix){
 }
 void setZero(vector<vector<int>>& matrix){
     for(int i = 0; i < matrix.size(); i++){
-        for(int j = 0; j < matrix.size(); j++){
+        for(int j = 0; j < matrix[0].size(); j++){
             if(matrix[i][j] == -1){
                 matrix[i][j] = 0;
             }
@@ -39,6 +42,74 @@ void setZero(vector<vector<int>>& matrix){
 }
 
 //BETTER SOLUTION
+vector<vector<int>> setZeroMatrix(vector<vector<int>>&matrix){
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    int row[m] = {0};
+    int col[n] = {0};
+
+    for(int i = 0; i < matrix.size(); i++){
+        for(int j = 0; j < matrix[0].size(); j++){
+            if(matrix[i][j] == 0){
+                row[i] = 1;
+                col[j] = 1; 
+            }
+        }
+    }
+
+    for(int i = 0; i < matrix.size(); i++){
+        for(int j = 0; j < matrix[0].size(); j++){
+            if(row[i] == 1 || col[j] == 1){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+//OPTIMAL SOLUTION
+vector<vector<int>> OptimalSolution(vector<vector<int>>& matrix){
+    int col = 1;
+
+    for(int i = 0; i < matrix.size(); i++){
+        for(int j = 0; j < matrix[0].size(); j++){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0;
+                if(j != 0){
+                    matrix[0][j] = 0;
+                }else{
+                    col = 1;
+                }
+            }
+        }
+    }
+
+    for(int i = 1; i <matrix.size(); i++){
+        for(int j = 1; j < matrix[0].size(); j++){
+            if(matrix[i][j] != 0){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if(matrix[0][0] == 0){
+        for(int j = 0; j < matrix[0].size(); j++){
+            matrix[0][j] = 0;
+        }
+    }
+
+    if(col == 0){
+        for(int i = 0; i < matrix.size(); i++){
+            matrix[i][0] = 0;
+        }
+    }
+
+    return matrix;
+}
 
 int main(){
 
@@ -51,18 +122,22 @@ int main(){
 
     // setMinusone(matrix);
     // setZero(matrix);
+
+    // setZeroMatrix(matrix);
+
+    OptimalSolution(matrix);
     for(int i = 0; i < matrix.size(); i++){
-        for(int j = 0; j < matrix.size(); j++){
+        for(int j = 0; j < matrix[0].size(); j++){
             cout<<matrix[i][j]<<" ";
         }cout<<endl;
     }
 
     
-    for(int i = 0; i < matrix.size(); i++){
-        for(int j = 0; j < matrix.size(); j++){
-            cout<<matrix[i][j]<<" ";
-        }cout<<endl;
-    }
+    // for(int i = 0; i < matrix.size(); i++){
+    //     for(int j = 0; j < matrix.size(); j++){
+    //         cout<<matrix[i][j]<<" ";
+    //     }cout<<endl;
+    // }
 
 
 
